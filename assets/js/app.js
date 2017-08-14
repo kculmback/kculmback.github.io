e7e10e78f895c5bc869a3137509fa270fb804128
kculmback <me@kaseyculmback.com>
2017-08-13 23:36:30 -0700

    add js for menu animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,18 @@
 // APP.JS
 // --------------------------------------------------
 
+var menuOpen = document.getElementById('menu-open');
+var menu = document.getElementById('main-menu');
+var menuClose = document.getElementById('menu-close');
+
+menuOpen.addEventListener('click', function() {
+	menu.classList.remove('closed');
+	menu.classList.add('opened');
+});
+menuClose.addEventListener('click', function() {
+	menu.classList.remove('opened');
+	setTimeout(function() {
+		menu.classList.add('closed');
+	}, 600)
+});
+
